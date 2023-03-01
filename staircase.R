6c49372cfdb90d029753c3aa98a83ecf2c7a5b98
AndrewKhassapov <Andrew.Khassapov@gmail.com>
2023-03-02 01:07:08 +1100

    Update staircase.R
    + Updated the 'Staircase' solution with comments to describe the flow.

--- staircase.R
+++ staircase.R
@@ -2,10 +2,10 @@
 # in R
 # Source: https://www.hackerrank.com/challenges/staircase/problem
 
-# Prints a staircase.
+# Prints a staircase pattern.
 # in R.
 # @params n: The number of rows in the staircase.
-# Prints: A right-aligned staircase of height 'n' and width 'n'.
+# Prints: A right-aligned staircase pattern of height 'n' and width 'n'.
 # @see cat() : https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/cat
 # @see sprintf() for C-style string formatting : https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/sprintf
 staircase <- function(n) {
@@ -14,8 +14,12 @@ staircase <- function(n) {
     arr <- c()
 
     for (i in 1:n) {
-        str <- sprintf("%*s\n", n, strrep(CHAR, i))
-        arr <- append(arr, cat(str))
+        # Repeat the character 'i' times
+        steps <- strrep(CHAR, i)
+        # Set width of string to 'n', to right-align the steps
+        row <- sprintf("%*s\n", n, steps)
+        # Use cat() to clear default formatting of: [1] "<STRING>"
+        arr <- append(arr, cat(row))
     }
 
     cat(arr)
