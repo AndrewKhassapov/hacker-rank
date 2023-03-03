5b040a21f6949d5a307b636a18ca726c88b3f8d9
AndrewKhassapov <Andrew.Khassapov@gmail.com>
2023-03-03 11:46:59 +1100

    Create time-conversion.R
    + Added "Time Conversion" solution.
    Converts 12-hour AM/PM time to 24-hour time.

---
+++ time-conversion.R
@@ -0,0 +1,11 @@
+# Solution to 'Time Conversion'
+# in R
+# Source: https://www.hackerrank.com/challenges/time-conversion/problem
+
+# Converts 12-hour AM/PM time to 24-hour (military) time.
+# @params s: Valid time in 12-hour format (hh:mm:ssAM or hh:mm:ssPM). eg. 12:01:00PM
+# @returns: {string} Time in 24-hour format. eg. 19:05:45
+timeConversion <- function(s) {
+    time <- format(strptime(c(s), "%I:%M:%S %p"), "%H:%M:%S")
+    return(time)
+}
