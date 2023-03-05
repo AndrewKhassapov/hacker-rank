859a75b819fc511f631e8e4b7d4643c23ed07e61
AndrewKhassapov <Andrew.Khassapov@gmail.com>
2023-03-05 16:17:49 +1100

    Added "Designer PDF Viewer" solution.

---
+++ designer-pdf-viewer.R
@@ -0,0 +1,27 @@
+# Solution to 'Designer PDF Viewer'
+# in R
+# Source: https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+
+# Returns the area of the highlighted area when selecting a contiguous block of text in a "PDF viewer".
+# Arrays in R are 1-indexed.
+# @params h: {array} Array of 26 integers representing the heights of each letter.
+# @params word: {string} A word consisting of lowercase English alphabetic letters.
+# @returns: {string} The highlight area of the word.
+designerPdfViewer <- function(h, word) {
+    ASCII_a_DEC <- 97 # Decimal ASCII value of 'a'
+    wordLn <- nchar(word)
+    if (wordLn == 0) {
+        return(0)
+    }
+    strArr <- utf8ToInt(word)
+    maxHeight <- -Inf
+
+    for (i in 1:length(strArr)) {
+        height <- h[strArr[i] - ASCII_a_DEC + 1]
+        if (height > maxHeight) {
+            maxHeight <- height
+        }
+    }
+
+    return(maxHeight * wordLn)
+}
