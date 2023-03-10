627b20c1315e98029f9e3995837a8627b656b43f
AndrewKhassapov <Andrew.Khassapov@gmail.com>
2023-03-10 17:28:41 +1100

    Update grading-students.R
    + Added comments to solution for 'Grading Students'.

--- grading-students.R
+++ grading-students.R
@@ -1,12 +1,17 @@
 # Solution to 'Grading Students'
 # in R
-# Source:
+# Source: https://www.hackerrank.com/challenges/grading/problem
 
-#
+# Returns an array of integers representing the rounded grades.
+# @params grades: {array} Array of integers before rounding.
+# returns: {array} Array of integers after rounding.
 gradingStudents <- function(grades) {
     return(lapply(grades, FUN = roundUp))
 }
 
+# Returns numbers rounded up to the nearest 5, if the result is 40 and above.
+# @params grade: {integer} A number to be rounded up.
+# @returns: {integer} The rounded up number.
 roundUp <- function(grade) {
     if (grade > 37) {
         gradeRounded <- 5 * ceiling(grade / 5)
