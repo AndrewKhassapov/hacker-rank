56d03f1788a30c723bcd93da41b25ca0e1c147c7
AndrewKhassapov <Andrew.Khassapov@gmail.com>
2023-03-19 02:27:37 +1100

    Update migratory-birds.R
    + Added description to solution for 'Migratory Birds'.

--- migratory-birds.R
+++ migratory-birds.R
@@ -1,3 +1,11 @@
+# Solution to 'Migratory Birds'
+# in R
+# Source: https://www.hackerrank.com/challenges/migratory-birds/problem
+
+# Returns the smallest mode of the array
+# Arrays in R are 1-indexed
+# @params arr: {arra} an array of integers representing types of birds sighted
+# @returns: {integer} the smallest mode. The lowest type number of the most common bird
 migratoryBirds <- function(arr) {
     t <- table(arr)
     mode <- names(t[t == max(t)])
