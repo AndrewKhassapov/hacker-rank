41d586b2ee234f9a84a77b3ec3fb5f08b4627dc0
AndrewKhassapov <Andrew.Khassapov@gmail.com>
2023-03-18 22:52:25 +1100

    Update between-two-sets.R
    + Added description to 'Between Two Sets' solution.

--- between-two-sets.R
+++ between-two-sets.R
@@ -7,10 +7,14 @@
 # @params b: {array} All multiples of numbers considered.
 # @returns: {integer} Count of integers that are between the sets.
 getTotalX <- function(a, b) {
-    start <- max(a)
-    limit <- min(b)
+    start <- max(a) # Number must be greater than or equal to largest factor
+    limit <- min(b) # Number must be less than or equal to smallest multiple
     numbers <- list()
 
+    # Brute force O(n^2) solution.
+    # Check if all of b is divisible by the number.
+    # Check if the number is divisible by all of a.
+    # Add the number to our list if conditions true.
     for (i in start:limit) {
         if (sum(b %% i) == 0) {
             if (sum(i %% a) == 0) {
