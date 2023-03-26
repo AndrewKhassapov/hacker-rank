8937609d1077da6c986a23d73b2ca7b2d77f97da
AndrewKhassapov <Andrew.Khassapov@gmail.com>
2023-03-27 00:09:49 +1100

    Documentation
    + Updated documentation for "Bill Division" solution.
    + Updated documentation for "Migratory Birds" solution.

--- bill-division.R
+++ bill-division.R
@@ -1,3 +1,12 @@
+# Solution to 'Bill Division'
+# in R
+# Source: https://www.hackerrank.com/challenges/bon-appetit/problem
+
+# Prints the amount of money that Brian owes Anna. If Brian did not overcharge Anna, prints Bon Appetit.
+# Arrays in R are 1-indexed
+# @params bill: {array} an array of the cost of each item ordered
+# @params k: {integer} the zero-based index of the item Anna doesn't eat
+# @param b: {integer} how much Anna pays for her share of the bill
 bonAppetit <- function(bill, k, b) {
     charged <- sum(bill) / 2
     actual <- sum(bill[-c(k + 1)]) / 2
--- migratory-birds.R
+++ migratory-birds.R
@@ -4,7 +4,7 @@
 
 # Returns the smallest mode of the array
 # Arrays in R are 1-indexed
-# @params arr: {arra} an array of integers representing types of birds sighted
+# @params arr: {array} an array of integers representing types of birds sighted
 # @returns: {integer} the smallest mode. The lowest type number of the most common bird
 migratoryBirds <- function(arr) {
     t <- table(arr)
