400df40bb6d377db44613c02d21c91db382b4a04
AndrewKhassapov <Andrew.Khassapov@gmail.com>
2023-04-02 01:11:48 +1100

    Update sales-by-match.R
    + Added description to solution for 'Sales By Match'.

--- sales-by-match.R
+++ sales-by-match.R
@@ -1,3 +1,12 @@
+# Solution to 'Sales by Match'
+# in R
+# Source: https://www.hackerrank.com/challenges/sock-merchant/problem
+
+# Returns the number of pairs
+# Arrays in R are 1-indexed
+# @params n: {integer} the number of items in the pile
+# @params ar: {array} an array with each value representing a unique item
+# @returns: {integer} the number of pairs
 sockMerchant <- function(n, ar) {
     frequency <- as.data.frame(table(ar))$Freq # table()$Freq lists how often each value appears.
     pairs <- 0
