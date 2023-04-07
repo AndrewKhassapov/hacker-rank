4a49eb6f6eaa4ca79c7734b551c7723cb732fa51
AndrewKhassapov <Andrew.Khassapov@gmail.com>
2023-04-08 00:10:38 +1000

    Update counting-valleys.R
    + Added description to solution "Counting Valleys".

--- counting-valleys.R
+++ counting-valleys.R
@@ -1,3 +1,15 @@
+# Solution to 'Counting Valleys'
+# in R
+# Source: https://www.hackerrank.com/challenges/counting-valleys/problem
+
+# Returns the number of valleys traversed
+# @params steps: {integer} the number of steps taken
+# @params path: {string} the path taken where U = up and D = down. Expecting "U" and "D" only.
+# eg. "UDDDUDUU" represents 1 valley traversed as:
+# _/\      _
+#    \    /
+#     \/\/
+# @returns: {integer} the number of valleys traversed
 countingValleys <- function(steps, path) {
     if (steps <= 1) {
         return(0)
