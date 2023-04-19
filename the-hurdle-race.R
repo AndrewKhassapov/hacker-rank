807b411c660ad6c556b8692c742e11131c09cc3f
AndrewKhassapov <Andrew.Khassapov@gmail.com>
2023-04-20 00:41:21 +1000

    Added solution to 'The Hurdle Race', in R.

---
+++ the-hurdle-race.R
@@ -0,0 +1,12 @@
+#' Solution to 'The Hurdle Race'
+#' in R
+#' Source: https://www.hackerrank.com/challenges/the-hurdle-race/problem
+
+#' Returns the difference between maximum hurdle height and jump height
+#' @param k: {integer} Jump height
+#' @param height: {array} Array of hurdle heights
+#' @returns: {integer} The difference between maximum hurdle height and jump height
+camelcase <- function(s) {
+   potion_drinks <- max(height) - max(k)
+   return(ifelse(potion_drinks > 0, potion_drinks, 0))
+}
